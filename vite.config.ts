b5ec5d25c1f12863003abd94314febd4646c19cf
pradeep-08 <114543921+pradeep-08@users.noreply.github.com>
2024-12-29 22:06:42 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/sujihairandmakeup",
+  base: "/suji_production_ui",
   optimizeDeps: {
     exclude: ["lucide-react"],
   },
