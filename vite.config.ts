e65b54340ca5a1ac88a3c8550ff8b0057d4eae7e
pradeep-08 <114543921+pradeep-08@users.noreply.github.com>
2024-12-29 22:25:17 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/suji_production_ui",
+  // base: "/suji_production_ui",
   optimizeDeps: {
     exclude: ["lucide-react"],
   },
